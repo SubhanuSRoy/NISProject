506592247f741e8347b18d873cb364282983c16b
Subhanu Sankar Roy <71728589+SubhanuSRoy@users.noreply.github.com>
2023-04-13 14:48:12 +0530

    ft. changed IP address to 223.228.143.76

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,5 +1,1 @@
-# Save the trained model as a pickle file
-pickle.dump(clf, open('dt_model.pkl', 'wb'))
-
-# joblib.dump(clf, "random_forest_model.pkl")
-print("Model saved as dt_model.pkl")
+223.228.143.7
